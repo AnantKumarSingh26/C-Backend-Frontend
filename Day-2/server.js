309762985cbb6db054e38a63a5f535d21779f9ed
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-06-20 16:30:15 +0530

    Home page and about page path set

--- Day-2/server.js
+++ Day-2/server.js
@@ -4,7 +4,11 @@ const app = express(); //Server Instance created
 
 app.get('/', (req,res)=>{
     console.log('Hello World');
-    res.send("Hello !!!")
+    res.send("Hello from Home page !!!")
+})
+app.get('/about', (req,res)=>{
+    console.log('Hello World');
+    res.send("Hello from About Page !!!")
 })
 
 app.listen(3000) // Server Start at 3000
