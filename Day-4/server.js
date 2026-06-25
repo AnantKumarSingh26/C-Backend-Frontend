dec724901e58fd8b9af68be34a31825119f0904b
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-06-25 11:44:03 +0530

    get and post method done in app.js

--- Day-4/server.js
+++ Day-4/server.js
@@ -1,8 +1,14 @@
 const app = require('./src/app')
 
-app.get('/',(req,res)=>{
-    res.send("Home Page")
-})
+// app.get('/',(req,res)=>{
+//     res.send("Home Page")
+// })
+
+// app.post('/notes',(req,res)=>{
+//         console.log(req.body);
+        
+//         res.send("Notes Created")
+// })
 
 app.listen(3000, ()=>{
     console.log('Server running on PORT-3000');
--- Day-4/src/app.js
+++ Day-4/src/app.js
@@ -2,4 +2,23 @@ const express = require("express")
 
 const app = express();
 
+app.use(express.json())
+
+const notes = [
+
+]
+
+app.get("/",(req,res)=>{
+    res.send("HomePage : Server is running Live")
+})
+
+app.post("/notes",(req,res){
+    console.log(req.body);
+
+    notes.push(req.body)
+
+    console.log(notes);
+
+    res.send("notes created")    
+})
 module.exports =app
