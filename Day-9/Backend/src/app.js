28f72aed81dc797f5160bb86ca28d48e4fc3cdb1
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-06-30 22:04:57 +0530

    FrontEnd added with fetching data from DB

--- Day-9/Backend/src/app.js
+++ Day-9/Backend/src/app.js
@@ -1,24 +1,59 @@
 const express = require('express')
 const noteModel = require('./models/note.model')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 // Post method
 
-app.post('/api/notes',async(req,res)=>{
-    const{title , description} = req.body
+app.post('/api/notes', async (req, res) => {
+    const { title, description } = req.body
 
-    const note =await noteModel.create({
-        title,description
+    const note = await noteModel.create({
+        title, description
     })
     res.status(201).json({
-        message:"Note Created Successfully",
+        message: "Note Created Successfully",
         note
     })
 })
 
+// GET Method to fetch all the data from MongoDB
 
+app.get('/api/notes', async (req, res) => {
+    const notes = await noteModel.find()
+    res.status(200).json({
+        message: "Notes Fetched Successfully",
+        notes
+    })
+})
+
+
+// Delete method to delete data from DB
+
+app.delete('/api/notes/:id', async (req, res) => {
+    const id = req.params.id;
+
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message: "Note Deleted Successfull y"
+    })
+})
+
+
+app.patch('/api/notes/:id', async (req, res) => {
+    const id = req.params.id;
+    const { description } = req.body;
+
+    await noteModel.findByIdAndUpdate(id, { description })
+
+    res.status(200).json({
+        message:"Note Updated Successfully"
+    })
+})
 
 
 module.exports = app;
