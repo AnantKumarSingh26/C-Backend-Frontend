c3f5e65be1bd6cce9bd56d0e25aadbe8118bd285
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-06-30 09:00:52 +0530

    Server-Connection, MongoDB, Express, dotenv, fixed DNS for error fixation while connectiong with MongoDB(Compass)

---
+++ Day-9/Backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config() //require to use dotenv data here
+// Starting Sever and Connecting to Database
+const app = require('./src/app')
+const connectToDB = require("./src/config/database")
+
+//! For server error DNS problem
+const dns = require('dns')
+dns.setServers(["1.1.1.1" ,"8.8.8.8"])
+connectToDB()
+
+
+app.listen(3000,(req,res)=>{
+    console.log('Server Running on PORT 3000');
+    
+})
