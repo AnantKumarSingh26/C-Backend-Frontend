ff0f1b4bcf888c9fa2ef348ae8ffda5a947c2535
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-01 08:08:57 +0530

    Added post method to add new notes to DB

--- Day-9/Frontend/src/App.jsx
+++ Day-9/Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
@@ -7,21 +7,42 @@ function App() {
   const [notes, setNotes] = useState([
     {
       title: "Loading....",
-      
+
     }
   ])
-  function fetchNotes(){
-       axios.get('http://localhost:3000/api/notes').then((res)=>{
-        setNotes(res.data.notes)
-      })
+  function fetchNotes() {
+    axios.get('http://localhost:3000/api/notes').then((res) => {
+      setNotes(res.data.notes)
+    })
   }
 
-  useEffect(()=>{   //it is used to stop continious rendering of setNotes in notes 
-   fetchNotes();
-  },[])
+  function handleSubmit(e) {
+    e.preventDefault()
+    const { title, description } = e.target.elements
+    console.log(title, description);
+    axios.post("http://localhost:3000/api/notes", {
+      title: title.value,
+      description: description.value
+    }).then(res=>{
+      console.log(res.data);
+      fetchNotes();
+      alert("Note Created Successfully")
+
+    })
+
+  }
+
+  useEffect(() => {   //it is used to stop continious rendering of setNotes in notes 
+    fetchNotes();
+  }, [])
 
   return (
     <>
+      <form action="" className="note-create-form" onSubmit={handleSubmit}>
+        <input name="title" type="text" placeholder="Enter Title" />
+        <input name="description" type="text" placeholder="Enter Description" />
+        <button>Create Note</button>
+      </form>
       <div className="notes">
         {
           notes.map(note => {
