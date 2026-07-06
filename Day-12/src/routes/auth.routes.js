daac0cdfe3d9f20e104b575b20e38d227fa78328
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-06 15:29:30 +0530

    added jwt token and saved token in cookie

--- Day-12/src/routes/auth.routes.js
+++ Day-12/src/routes/auth.routes.js
@@ -4,23 +4,32 @@ const jwt = require('jsonwebtoken')
 
 const authRouter = express.Router()
 
-authRouter.post('/register',async(req,res)=>{
-    const {email, name,password} = req.body
+authRouter.post('/register', async (req, res) => {
+    const { email, name, password } = req.body
 
-    const isUserAlreadyExist = await userModel.findOne({email})
+    const isUserAlreadyExist = await userModel.findOne({ email })
 
-    if(isUserAlreadyExist){
+    if (isUserAlreadyExist) {
         return res.status(400).json({
             message: 'User already exists with this email address'
         })
     }
 
-    const user =await userModel.create({
-        email,password,name
+    const user = await userModel.create({
+        email, password, name
     })
+
+    const token = jwt.sign(
+        {
+            id: user._id
+        },
+        process.env.JWT_SECRET
+    )
+    res.cookie("jwt-token",token)
     res.status(201).json({
-        message:'User Registered Successfully',
-        user
+        message: 'User Registered Successfully',
+        user,
+        token
     })
 })
 
