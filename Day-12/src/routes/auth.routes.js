d6972090d9e750459e5eccd977bfc14cf2d21579
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-07 12:48:00 +0530

    password hash used

--- Day-12/src/routes/auth.routes.js
+++ Day-12/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const userModel = require('../models/user.model')
 const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
 
 const authRouter = express.Router()
 
@@ -14,9 +15,9 @@ authRouter.post('/register', async (req, res) => {
             message: 'User already exists with this email address'
         })
     }
-
+    const hash = crypto.createHash('md5').update(password).digest('hex')
     const user = await userModel.create({
-        email, password, name
+        email, password:hash, name
     })
 
     const token = jwt.sign(
@@ -25,18 +26,54 @@ authRouter.post('/register', async (req, res) => {
         },
         process.env.JWT_SECRET
     )
-    res.cookie("jwt-token", token)
+    res.cookie("jwt_token", token)
     res.status(201).json({
         message: 'User Registered Successfully',
         user,
         token
     })
 })
+// /api/auth/protected
 authRouter.post('/protected',async(req,res)=>{
     console.log(req.cookies)
 
     res.status(200).json({
         message:"This is protected route"
+    })
+})
+// /api/auth/login
+// !    Controller
+
+authRouter.post('/login',async(req,res)=>{
+    const{email,password}=req.body
+
+    const user = await userModel.findOne({email})
+
+    if(!user){
+        return res.status(404).json({
+            message: "User not found with this email address"
+        })
+    }
+
+    const isPasswordMatched = user.password === crypto.createHash('md5').update(password).digest('hex')
+
+    if(!isPasswordMatched){
+        return res.status(401).json({
+            message: "Invalid Password"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+    },
+    process.env.JWT_SECRET)
+
+    res.cookie("jwt_token",token)
+
+    res.status(200).json({
+        message: "User Logged in Successfully",
+        user,
+        token
     })
 })
 
