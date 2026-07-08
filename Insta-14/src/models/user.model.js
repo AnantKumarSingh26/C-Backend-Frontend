cdeb0f82849e531d49a85a7d8f0b7b362d33ad98
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-08 23:24:16 +0530

    User model or schema of database

---
+++ Insta-14/src/models/user.model.js
@@ -0,0 +1,28 @@
+  const mongoose = require('mongoose')
+
+  const userSchema = new mongoose({
+    username:{
+        type:String,
+        unique: [true,"Username already exist"],
+        required:[true,"Username required"]
+    },
+    email:{
+        type:String,
+        unique:[true,"Email already exist"],
+        required:[true, "Email is required"]
+    },
+    password:{
+        type:String,
+        required:[true,"Password Required to continue"]
+    },
+    bio:String,
+    profileImage:{
+        type:String,
+        default:"https://ik.imagekit.io/v3gzubezm/default-image.jpg?updatedAt=1783532903340"
+    },
+
+  })
+
+  const userModel= mongoose.model("Users",userSchema)
+
+  module.exports = userModel
