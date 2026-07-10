0659a99b34dfc43656fe5ce01a15b29dd8291f42
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-10 23:11:00 +0530

    Good way to check username and email with one request

--- Insta-14/src/routes/auth.route.js
+++ Insta-14/src/routes/auth.route.js
@@ -7,16 +7,28 @@ const authRouter = express.Router()
 authRouter.post('/register',async(req, res)=>{
     const {email,username, password,bio,profileImage}= req.body
 
-    const isUserExistByEmail = await userModel.findOne({email})
-    if(isUserExistByEmail){
+    // const isUserExistByEmail = await userModel.findOne({email})
+    // if(isUserExistByEmail){
+    //     return res.status(409).json({
+    //         message:"User already exist with email"
+    //     })
+    // }
+    // const isUserExistByUsername = await userModel.findOne({username})
+    // if(isUserExistByUsername){
+    //     return res.status(409).json({
+    //         message:"Choose different username"
+    //     })
+    // }
+
+    const isUserAlreadyExist = await  userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(isUserAlreadyExist ){
         return res.status(409).json({
-            message:"User already exist with email"
-        })
-    }
-    const isUserExistByUsername = await userModel.findOne({username})
-    if(isUserExistByUsername){
-        return res.status(409).json({
-            message:"Choose different username"
+            message : "User Already Exists"
         })
     }
 })
