e623d046863dee3e2f0de06ec653d660daa1c88a
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-13 17:13:57 +0530

    Controller added

---
+++ Insta-14/src/controllers/auth.controllers.js
@@ -0,0 +1,110 @@
+const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/user.model')
+
+//! REGISTER
+async function registerController(req, res) {
+    const { email, username, password, bio, profileImage } = req.body
+
+    // const isUserExistByEmail = await userModel.findOne({email})
+    // if(isUserExistByEmail){
+    //     return res.status(409).json({
+    //         message:"User already exist with email"
+    //     })
+    // }
+    // const isUserExistByUsername = await userModel.findOne({username})
+    // if(isUserExistByUsername){
+    //     return res.status(409).json({
+    //         message:"Choose different username"
+    //     })
+    // }
+
+    const isUserAlreadyExist = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+    if (isUserAlreadyExist) {
+        return res.status(409).json({
+            message: "User Already Exists" + (isUserAlreadyExist.email == email ? " Email already exist" : "Username already exist")
+        })
+    }
+
+    const hash = crypto.createHash('sha256').update(password).digest('hex')
+
+    const user = await userModel.create({
+        username,
+        email,
+        bio,
+        profileImage,
+        password: hash
+    })
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET, { expiresIn: "1d" })
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            profileImage: user.profileImage
+        }
+    })
+}
+
+//! LOGIN
+async function loginController(req, res) {
+    const { username, email, password } = req.body
+
+    const user = await userModel.findOne({
+        $or: [
+            {
+                username: username,
+            },
+            {
+                email: email,
+            }
+        ]
+    })
+    if(!user){
+        return res.status(404).json({
+            message:"User Not Found"
+        })
+    }
+    const hash = crypto.createHash("sha256").update(password).digest('hex')
+
+    const isPasswordValid = hash == user.password
+
+    if(!isPasswordValid){
+        return res.status(404).json({
+            message:"Password Invalid"
+        })
+    }
+
+    const token = jwt.sign(
+        {id:user._id},
+        process.env.JWT_SECRET,
+        {
+            expiresIn:"1d"
+        }
+    )
+    res.cookie("token",token)
+
+    res.status(200).json({
+        message:"User Logged In Successfully",
+        user:{
+            username:user.username,
+            email:user.email
+        }
+    })
+}
+
+module.exports ={
+    registerController,
+    loginController
+}
--- Insta-14/src/routes/auth.route.js
+++ Insta-14/src/routes/auth.route.js
@@ -1,109 +1,11 @@
 const express = require('express')
-const crypto = require('crypto')
-const jwt = require('jsonwebtoken')
-const userModel = require('../models/user.model')
+const authController = require("../controllers/auth.controllers")
 
 const authRouter = express.Router()
 
 
-authRouter.post('/register', async (req, res) => {
-    const { email, username, password, bio, profileImage } = req.body
+authRouter.post('/register', authController.registerController)
 
-    // const isUserExistByEmail = await userModel.findOne({email})
-    // if(isUserExistByEmail){
-    //     return res.status(409).json({
-    //         message:"User already exist with email"
-    //     })
-    // }
-    // const isUserExistByUsername = await userModel.findOne({username})
-    // if(isUserExistByUsername){
-    //     return res.status(409).json({
-    //         message:"Choose different username"
-    //     })
-    // }
-
-    const isUserAlreadyExist = await userModel.findOne({
-        $or: [
-            { username },
-            { email }
-        ]
-    })
-    if (isUserAlreadyExist) {
-        return res.status(409).json({
-            message: "User Already Exists" + (isUserAlreadyExist.email == email ? " Email already exist" : "Username already exist")
-        })
-    }
-
-    const hash = crypto.createHash('sha256').update(password).digest('hex')
-
-    const user = await userModel.create({
-        username,
-        email,
-        bio,
-        profileImage,
-        password: hash
-    })
-    const token = jwt.sign({
-        id: user._id
-    }, process.env.JWT_SECRET, { expiresIn: "1d" })
-
-    res.cookie("token", token)
-
-    res.status(201).json({
-        message: "User registered successfully",
-        user: {
-            id: user._id,
-            username: user.username,
-            email: user.email,
-            profileImage: user.profileImage
-        }
-    })
-})
-
-authRouter.post('/login', async (req, res) => {
-    const { username, email, password } = req.body
-
-    const user = await userModel.findOne({
-        $or: [
-            {
-                username: username,
-            },
-            {
-                email: email,
-            }
-        ]
-    })
-    if(!user){
-        return res.status(404).json({
-            message:"User Not Found"
-        })
-    }
-    const hash = crypto.createHash("sha256").update(password).digest('hex')
-
-    const isPasswordValid = hash == user.password
-
-    if(!isPasswordValid){
-        return res.status(404).json({
-            message:"Password Invalid"
-        })
-    }
-
-    const token = jwt.sign(
-        {id:user._id},
-        process.env.JWT_SECRET,
-        {
-            expiresIn:"1d"
-        }
-    )
-    res.cookie("token",token)
-
-    res.status(200).json({
-        message:"User Logged In Successfully",
-        user:{
-            username:user.username,
-            email:user.email
-        }
-    })
-})
+authRouter.post('/login', authController.loginController)
 
 module.exports = authRouter
