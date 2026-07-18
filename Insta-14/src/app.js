25f3080f2b02dd59b4f6f4fe00e11fe8bb944f67
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-18 12:48:41 +0530

    adding checks for users for following and followee two done self following disabled and if existing following stop for one more time following that username

--- Insta-14/src/app.js
+++ Insta-14/src/app.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const cookieParser = require('cookie-parser');
-const authRouter = require('./routes/auth.route');
-const postRouter = require('./routes/post.routes')
 
 
 const app = express();
@@ -9,7 +7,12 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 
+const authRouter = require('./routes/auth.route');
+const postRouter = require('./routes/post.routes')
+const userRouter = require('./routes/user.routes')
+
 app.use("/api/auth",authRouter)
 app.use("/api/post",postRouter)
+app.use("/api/users",userRouter)
 
 module.exports = app;
--- Insta-14/src/routes/user.routes.js
+++ Insta-14/src/routes/user.routes.js
@@ -1,7 +1,13 @@
 const express = require('express')
-const userRouter = express.Router()
 const userController = require('../controllers/user.controller')
+const userRouter = express.Router()
+const identifyUser = require('../middleware/auth.middleware')
+
+// route post /api/user/follow/:userid 
+//! @desc Follow a user
+// ? access : private
+
+userRouter.post('/follow/:username', identifyUser, userController.followUserController)
 
 
-
-module.exports= userRouter;
+module.exports = userRouter;
