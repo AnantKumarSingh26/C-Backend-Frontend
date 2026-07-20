c56d092d4b5c1704326e2f541b230d035b5f28d7
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-20 10:06:38 +0530

    cors middleware used and credentials set up for setting up browser cookie

--- Insta-14/Frontend/src/Features/auth/pages/Register.jsx
+++ Insta-14/Frontend/src/Features/auth/pages/Register.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
-import {Link} from 'react-router'
+import React, { useState } from 'react'
+import { Link } from 'react-router'
 import '../styles/form.scss'
+import axios from "axios";
+
 const Register = () => {
+  //Two way Binding
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("second")
+
+  async function handleFormSubmit(e) {
+    e.preventDefault()
+    axios.post("http://localhost:3000/api/auth/register", {
+      username, email, password,
+    },{withCredentials:true}).then(res =>{
+      console.log(res.data)
+    })
+  }
+
   return (
     <main>
       <div className="form-container">
         <h1>Register</h1>
-        <form action="">
-          <input type="text" name='username' placeholder='Enter Username' />
-          <input type="text" name='email' placeholder='Enter Email' />
-          <input type="text" name='password' placeholder='Enter Password' />
+        <form onSubmit={handleFormSubmit}>
+          <input onInput={(e) => { setUsername(e.target.value) }}
+            type="text" name='username' placeholder='Enter Username' />
+          <input onInput={(e) => { setEmail(e.target.value) }}
+            type="text" name='email' placeholder='Enter Email' />
+          <input onInput={(e) => { setPassword(e.target.value) }}
+            type="text" name='password' placeholder='Enter Password' />
           <button>Register</button>
         </form>
-      <p>Already have an account <Link className="toggleAuthForm" to='/login'>Login</Link></p>
+        <p>Already have an account <Link className="toggleAuthForm" to='/login'>Login</Link></p>
       </div>
     </main>
   )
