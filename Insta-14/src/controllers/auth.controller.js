379880575f2ae12c8d41f881c559d155420a0d4a
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-21 00:23:59 +0530

    get me api added

--- Insta-14/src/controllers/auth.controller.js
+++ Insta-14/src/controllers/auth.controller.js
@@ -32,7 +32,7 @@ async function registerController(req, res) {
         })
     }
 
-    const hash =await bcrypt.hash(password, 10)
+    const hash = await bcrypt.hash(password, 10)
 
     const user = await userModel.create({
         username,
@@ -73,43 +73,58 @@ async function loginController(req, res) {
             }
         ]
     })
-    if(!user){
+    if (!user) {
         return res.status(404).json({
-            message:"User Not Found"
+            message: "User Not Found"
         })
     }
-   
 
-    const isPasswordValid = await bcrypt.compare(password,user.password)
 
-    if(!isPasswordValid){
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
         return res.status(404).json({
-            message:"Password Invalid"
+            message: "Password Invalid"
         })
     }
 
     const token = jwt.sign(
         {
-            id:user._id,
+            id: user._id,
             username: user.username
         },
         process.env.JWT_SECRET,
         {
-            expiresIn:"1d"
+            expiresIn: "1d"
         }
     )
-    res.cookie("token",token)
+    res.cookie("token", token)
 
     res.status(200).json({
-        message:"User Logged In Successfully",
-        user:{
-            username:user.username,
-            email:user.email
+        message: "User Logged In Successfully",
+        user: {
+            username: user.username,
+            email: user.email
         }
     })
 }
 
-module.exports ={
+async function getMeController(req, res) {
+    const userId= req.user.id
+    const user = await userModel.findById(userId)
+
+    res.status(200).json({
+        user:{
+            username :user.username,
+            email: user.email,
+            bio: user.bio,
+            profileImage :user.profileImage
+        }
+    })
+}
+
+module.exports = {
     registerController,
-    loginController
+    loginController,
+    getMeController
 }
