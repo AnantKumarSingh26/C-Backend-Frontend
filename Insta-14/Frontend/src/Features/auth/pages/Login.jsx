2f4088aad5ba79bb3965e1819290f66892d330db
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-20 22:33:18 +0530

    backend+frontend login and register working

--- Insta-14/Frontend/src/Features/auth/pages/Login.jsx
+++ Insta-14/Frontend/src/Features/auth/pages/Login.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/form.scss'
-import {  Link} from "react-router";
-
+import { Link } from "react-router";
+import axios from "axios";
 const Login = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios.post("http://localhost:3000/api/auth/login", {
+      username, password
+    }, { withCredentials: true }
+    ).then(res => {
+      console.log(res.data)
+      alert('Logged In Successfully')
+    })
+  }
+
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
-        <form action="">
-          <input type="text" name="username" placeholder="Enter Username"/>
-          <input type="password" name="password" placeholder="Enter Password" />
+        <form onSubmit={handleSubmit}>
+          <input onInput={(e) => { setUsername(e.target.value) }}
+            type="text" name="username" placeholder="Enter Username" />
+          <input onInput={(e) => { setPassword(e.target.value) }}
+            type="password" name="password" placeholder="Enter Password" />
           <button type="submit">Submit</button>
         </form>
         <p>Don't have an account! <Link className="toggleAuthForm" to='/register'>Register</Link></p>
