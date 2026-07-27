6d6ecb227e63b85141fa677c7f577b058e77f2c3
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-07-27 09:58:01 +0530

    like button fixed for liked post

--- Insta-14/Backend/src/controllers/post.controller.js
+++ Insta-14/Backend/src/controllers/post.controller.js
@@ -98,7 +98,17 @@ async function likePostController(req, res) {
 }
 
 async function getFeedController(req, res) {
-    const posts = await postModel.find().populate("user").select("")
+    const user = req.user
+
+    const posts = await Promise.all((await postModel.find().populate("user").select("").lean())
+        .map(async (post) => {
+            const isLiked = await likeModel.findOne({
+                user: user.username,
+                post:post._id
+            })
+            post.isLiked = !!isLiked //Boolean(liked)
+            return post
+        }))
 
     res.status(200).json({
         message: "Posts Fetched Successfully",
