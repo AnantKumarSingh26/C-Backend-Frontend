43e8c30d996a6b906017384d92618e2376519eb3
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-08-06 09:29:07 +0530

    initialize Socket.IO project with Express server
    - Added package.json to manage dependencies and scripts
    - Created server.js to set up HTTP server and Socket.IO
    - Implemented basic Express app in app.js

---
+++ Day-31(Socket IO)/server.js
@@ -0,0 +1,19 @@
+import app from './src/app.js'
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  
+});
+
+
+httpServer.listen(3000,()=>{
+    console.log("Server is running on PORT 3000 ")
+});
+
+// app.listen(3000,()=>{
+//     console.log('Server running on PORT 3000')
+// })
